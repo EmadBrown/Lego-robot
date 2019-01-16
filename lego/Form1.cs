5bc0b227de10e32cfa8cf0a0b5147f378b802bad
i410456 <e.albouni@student.fontys.nl>
2019-01-16 11:41:57 +0100

    Star on

--- lego/Form1.cs
+++ lego/Form1.cs
@@ -6,9 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
 using System.Windows.Forms;
-using Lego.Ev3.Core;
-using Lego.Ev3.Desktop;
 using EV3MessengerLib;
 using Lego.Ev3.Core;
 using Lego.Ev3.Desktop;
@@ -21,10 +21,15 @@ namespace lego
         Brick bricka;
         EV3Messenger messenger;
         EV3Message message;
+        Stopwatch stopwatch;
+        Stopwatch stopwatchTimer = new Stopwatch();
+        Stopwatch stopwatchTimer2 = new Stopwatch();
+        Robot_States robot_States;
 
         public Form1()
         {
             InitializeComponent();
+            messenger = new EV3Messenger();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,6 +37,34 @@ namespace lego
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (messenger.Connect("COM7"))
+            {
+                MessageBox.Show("AUTOMATIC MODE ON");
+            }
+            else
+            {
+                MessageBox.Show("Can't Connect");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            robot_States = Robot_States.starting;
+            while (robot_States != Robot_States.paused)
+            {
+                message = messenger.ReadMessage();
+                if (message != null)
+                {
+                    //
+                }
+            }
+
+          // Start
+          //
+           //
+        }
     }
 
     public enum Robot_States
